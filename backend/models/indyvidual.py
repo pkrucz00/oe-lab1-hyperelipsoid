5b6e0d31475973cdea300792501c6004fa544696
karolk <karolkusper@gmail.com>
2025-03-22 23:20:48 +0100

    Change indyvidual to have only one chromosome

--- backend/models/indyvidual.py
+++ backend/models/indyvidual.py
@@ -1,1 +1,24 @@
-# Klasa Individual – osobnik składający się z chromosomów oraz przypisana wartość fitness
+from backend.models.chromosome import Chromosome
+
+class Individual:
+    def __init__(self, chromosome: Chromosome):
+        """
+        Inicjalizacja osobnika z pojedynczym chromosomem.
+        :param chromosome: instancja Chromosome
+        """
+        self.chromosome = chromosome
+        self.fitness = None  # wartość funkcji celu, ustalana później
+
+    def get_phenotype(self, a: float, b: float) -> float:
+        """
+        Zwraca zdekodowaną wartość (fenotyp) chromosomu.
+        :param a: dolny zakres poszukiwań
+        :param b: górny zakres poszukiwań
+        :return: wartość dziesiętna
+        """
+        return self.chromosome.decode(a, b)
+
+    def __str__(self):
+        phenotype = self.get_phenotype(-65.536, 65.536)
+        return f"Individual(phenotype={phenotype}, fitness={self.fitness})"
+
--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-from domain.problem_function import hyperelipsoid
+# from domain.problem_function import hyperelipsoid
 
-print(hyperelipsoid([1, 2, 3]))
+# print(hyperelipsoid([1, 2, 3]))
+
+
+from backend.models.chromosome import Chromosome
+from backend.models.indyvidual import Individual
+
+
+# Przykładowe użycie:
+if __name__ == "__main__":
+    # Generowanie losowego chromosomu o długości 10
+    chrom = Chromosome.random(10)
+    print("Losowy chromosom:", chrom)
+    
+    # Dekodowanie chromosomu w przedziale [-65.536, 65.536]
+    decoded_value = chrom.decode(-65.536, 65.536)
+    print("Zdekodowana wartość:", decoded_value)
+
+    # Tworzenie osobnika z pojedynczym losowym chromosomem
+    individual = Individual(Chromosome.random(10))
+    print("Osobnik:", individual)
