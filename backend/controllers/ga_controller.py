3b6c5092f596f2a8e717406b7c04b4b591a9250f
Pawel Kruczkiewicz <pawel.kruczkiewicz@klarna.com>
2025-03-24 07:52:22 +0100

    Adding plot and json persistance of results

--- backend/controllers/ga_controller.py
+++ backend/controllers/ga_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from backend.services.ga_service import run_ga
+from backend.services.io_service import persist_result
 
 ga_blueprint = Blueprint("ga", __name__, url_prefix="/api/ga")
 
@@ -7,4 +8,6 @@ ga_blueprint = Blueprint("ga", __name__, url_prefix="/api/ga")
 def run():
     config = request.get_json()
     result = run_ga(config)
+    persist_result(result)
+
     return jsonify(result)
