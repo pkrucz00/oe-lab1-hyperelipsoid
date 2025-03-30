096d418de458094cc68add53a08005817ab61ed6
Pawel Kruczkiewicz <pawel.kruczkiewicz@klarna.com>
2025-03-30 18:48:49 +0200

    Adding routes to static files and CHANGING URL

--- backend/controllers/ga_controller.py
+++ backend/controllers/ga_controller.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, send_from_directory
 from backend.services.ga_service import run_ga
 from backend.services.io_service import persist_result
 
-ga_blueprint = Blueprint("ga", __name__, url_prefix="/api/ga")
+ga_blueprint = Blueprint("ga", __name__)
 
 @ga_blueprint.route("/run", methods=["POST"])
 def run():
@@ -11,3 +11,19 @@ def run():
     persist_result(result)
 
     return jsonify(result)
+
+@ga_blueprint.route("/", methods=["GET"])
+def index():
+    """
+    Serve the index page.
+    :return: Rendered HTML page.
+    """
+    return send_from_directory('frontend', 'stronka.html')
+
+@ga_blueprint.route("/style.css", methods=["GET"])
+def send_csv():
+    """
+    Serve a file from the static folder.
+    :return: The file.
+    """
+    return send_from_directory('frontend', 'style.css')
